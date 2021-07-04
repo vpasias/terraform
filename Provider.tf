ae9d31d7b16dd6b12e932977a96d2611918f5209
vpasias <52623837+vpasias@users.noreply.github.com>
2021-07-04 10:58:19 +0300

    Create Provider.tf

---
+++ Provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+    }
+  }
+}
+
+provider "libvirt" {
+  uri = "qemu:///system"
+}
