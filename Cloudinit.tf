4bb8e319384da5bfe38c895406bf04045b0e3a77
vpasias <52623837+vpasias@users.noreply.github.com>
2021-07-04 10:59:17 +0300

    Create Cloudinit.tf

---
+++ Cloudinit.tf
@@ -0,0 +1,12 @@
+data "template_file" "user_data" {
+  template = file("user-data.yml")
+  vars = {
+    hostname = var.domain
+    sshdport = var.ssh_port
+  }
+}
+
+resource "libvirt_cloudinit_disk" "cloud-init" {
+  name           = "cloud-init.iso"
+  user_data      = data.template_file.user_data.rendered
+}
