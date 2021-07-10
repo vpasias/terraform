c03c3c617094178e4d8a6dd677e31ab81ab8a71c
vpasias <52623837+vpasias@users.noreply.github.com>
2021-07-10 13:05:45 +0300

    Update main.tf

--- lab3/main.tf
+++ lab3/main.tf
@@ -49,6 +49,7 @@ data "template_file" "user_data" {
   template = file("${path.module}/cloud_init.cfg")
   vars = {
     hostname = "${var.hostname}-${count.index}"
+    sshdport = "${var.sshdport}"    
   }
 }
 
